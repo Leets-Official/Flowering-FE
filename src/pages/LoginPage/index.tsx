14b4662aab51abbc65e3d4f830c497efb8c95d34
ggome1 <bookincar@naver.com>
2024-01-03 03:45:05 +0900

    feat: 카카오 로그인 인가 코드 받아오기

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,9 +1,24 @@
+import loginImage from '@/assets/kakao_login_medium_wide.png';
+
 const LoginPage = () => {
+
+  const LoginAssign = () => {
+    const REDIRECT_URI = 'http://localhost:5173/login/oauth';
+    const CLIENT_ID = '2d614c05b308f92b587cd4c0006c669d';
+
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`;
+    window.location.href = KAKAO_AUTH_URL;
+  }
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <img
+        src={loginImage}
+        onClick={LoginAssign}
+      />
     </div>
-  )
-}
+  );
+};
+
 
 export default LoginPage
