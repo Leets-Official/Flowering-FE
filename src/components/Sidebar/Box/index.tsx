5fa7c72f21192026597045123dd71207cb3f1613
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-07 16:57:26 +0900

    style: margin 제거 후 gap 변경

--- src/components/Sidebar/Box/index.tsx
+++ src/components/Sidebar/Box/index.tsx
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom'
 const Box = () => {
   return (
     <>
-      <div className="flex h-screen w-[315px] flex-col rounded-l-[50px] bg-white pl-10 pt-10">
+      <div className="flex h-screen w-[315px] flex-col gap-8 rounded-l-[50px] bg-white pl-10 pt-10">
         <Link to="/login">
-          <p className="mb-8 text-[22px]">로그인 후 사용하세요!</p>
+          <p className="text-[22px]">로그인 후 사용하세요!</p>
         </Link>
-        <Content content="MY" route="/" />
-        <Content content="SHOP" route="/" />
-        <Content content="LUCKY DRAW" route="/" />
-        <Content content="STORAGE" route="/" />
-        <Content content="MY FLING" route="/" />
+        <div className="flex flex-col gap-5">
+          <Content content="MY" route="/" />
+          <Content content="SHOP" route="/" />
+          <Content content="LUCKY DRAW" route="/" />
+          <Content content="STORAGE" route="/" />
+          <Content content="MY FLING" route="/" />
+        </div>
       </div>
     </>
   )
