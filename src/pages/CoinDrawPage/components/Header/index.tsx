555841e2c42dc78b061f67ff42a190d39d326572
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-07 17:27:35 +0900

    feat: CoinDraw페이지 Header 컴포넌트 구현

---
+++ src/pages/CoinDrawPage/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { HomeIcon } from '@/assets/Icons'
+import { useNavigate } from 'react-router-dom'
+
+const Header = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex items-center justify-between bg-gray-200 px-6 py-6">
+      <div className="text-xl font-bold text-[#5F5F5F]">LUCKY DRAW</div>
+      <i className="rounded-full border p-1 hover:border hover:border-gray-400 hover:bg-gray-300">
+        <HomeIcon className="cursor-pointer" onClick={() => navigate('/')} />
+      </i>
+    </div>
+  )
+}
+
+export default Header
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div className="border-x-1 mx-auto min-h-screen border-black bg-white">
+    <div className="border-x-1 mx-auto h-screen border-black bg-white">
       <Outlet />
     </div>
   )
