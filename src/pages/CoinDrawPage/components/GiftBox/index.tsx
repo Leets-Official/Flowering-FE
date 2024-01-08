12dde4fdbe093c3f09b4f6a1b8373ef18b537dc5
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-08 17:37:17 +0900

    feat: 랜덤 코인 뽑기 페이지 컴포넌트 전환 구현

--- src/pages/CoinDrawPage/components/GiftBox/index.tsx
+++ src/pages/CoinDrawPage/components/GiftBox/index.tsx
@@ -1,29 +1,47 @@
 import { useFrame, useLoader } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { Mesh, TextureLoader } from 'three'
 import { Box, PerspectiveCamera } from '@react-three/drei'
 
-const GiftBox = () => {
+interface GiftBoxProps {
+  haveTodayChance: boolean
+  setDrawingStatus: Dispatch<SetStateAction<'before' | 'loading' | 'after'>>
+}
+
+const GiftBox = ({ haveTodayChance, setDrawingStatus }: GiftBoxProps) => {
   const boxRef = useRef<Mesh>(null)
   const [isAnimating, setIsAnimating] = useState<boolean>(false)
-  const [scale, setScale] = useState<number>(4)
+  const [scale, setScale] = useState<number>(6)
 
   const texture = useLoader(
     TextureLoader,
     new URL('@/assets/images/wrapper.jpeg', import.meta.url).href,
   )
 
-  const handleClickBox = () => {
-    if (!isAnimating) {
-      setIsAnimating(true)
-      setScale(5) // 클릭 시 상자를 확대
+  const openBox = useCallback(() => {
+    setIsAnimating(true)
+    setScale(7) // 클릭 시 상자를 확대
+    setDrawingStatus('loading')
 
-      setTimeout(() => {
-        setIsAnimating(false)
-        setScale(4) // 애니메이션 종료 후 원래 크기로
-      }, 1500) // 1.5초 후에 애니메이션 종료
-    }
-  }
+    setTimeout(() => {
+      setIsAnimating(false)
+      setScale(6) // 애니메이션 종료 후 원래 크기로
+      setDrawingStatus('after')
+    }, 2000) // 1.5초 후에 애니메이션 종료
+  }, [setDrawingStatus])
+
+  useEffect(() => {
+    if (haveTodayChance) return
+
+    openBox()
+  }, [haveTodayChance, openBox])
 
   useFrame(({ clock }) => {
     if (boxRef.current) {
@@ -50,8 +68,7 @@ const GiftBox = () => {
       <Box
         ref={boxRef}
         args={[1, 1, 1]}
-        position={[0, 0.5, 1]}
-        onPointerDown={handleClickBox}
+        position={[0, -2, 2]}
         rotation={[0.4, -0.5, 0]}
       >
         <meshStandardMaterial map={texture} attach="material" />
--- src/pages/CoinDrawPage/index.tsx
+++ src/pages/CoinDrawPage/index.tsx
@@ -1,27 +1,79 @@
+import { Button } from '@/components'
 import { Header, GiftBox } from './components'
 import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
 
+// imsy 데이터
 const userName = '주하'
 const userCoin = 3000
 
 const CoinDrawPage = () => {
+  // imsy 데이터
+  const [haveTodayChance, setHaveTodayChance] = useState<boolean>(true)
+  const [drawingStatus, setDrawingStatus] = useState<
+    'before' | 'loading' | 'after'
+  >('before')
+
+  const handleClickButton = () => {
+    setHaveTodayChance(false)
+    setDrawingStatus('loading')
+  }
+
   return (
     <>
       <Header />
-      <div className="flex h-full flex-col bg-gray-200 px-6 py-2">
-        <div className="flex flex-col gap-2">
-          <div className="text-base font-light text-[#676767]">
+      <div className="relative flex h-full flex-col px-6 py-2">
+        <div
+          className={`${
+            drawingStatus !== 'before' && 'invisible'
+          } flex flex-col gap-2`}
+        >
+          <div className="text-base font-light text-[rgb(103,103,103)]">
             <p>{userName} 님이</p>
             <p>현재 보유하고 있는 코인</p>
           </div>
           <p className="text-xl font-bold">{userCoin.toLocaleString()} 코인</p>
         </div>
-        <div className="flex h-full">
-          <Canvas>
-            <ambientLight intensity={5} />
-            <pointLight position={[10, 10, 10]} />
-            <GiftBox />
-          </Canvas>
+        {drawingStatus === 'after' && (
+          <div className="absolute left-0 top-[10%] flex w-full flex-col items-center justify-center gap-2">
+            <h2>축하합니다!</h2>
+            <div className="flex flex-col gap-1.5 text-center">
+              <h1 className="text-xl font-bold">100 코인</h1>
+              <p className="text-[#d9d9d9]">상점에서 선물을 구매해보세요.</p>
+            </div>
+          </div>
+        )}
+        <div className="flex h-full flex-col justify-between">
+          <div className="flex min-h-[400px] shrink">
+            <Canvas>
+              <ambientLight intensity={5} />
+              <pointLight position={[10, 10, 10]} />
+              <GiftBox
+                haveTodayChance={haveTodayChance}
+                setDrawingStatus={setDrawingStatus}
+              />
+            </Canvas>
+          </div>
+          <div className="flex flex-col justify-center gap-[0.44rem] py-3">
+            {drawingStatus === 'before' && (
+              <>
+                <p className="text-center text-xs font-light text-[#959595]">
+                  {haveTodayChance ? '1/1' : '0/1'}
+                </p>
+                <Button
+                  className="w-full rounded-[3.125rem] border"
+                  onClick={handleClickButton}
+                >
+                  {haveTodayChance ? '코인 뽑기' : '기회 끝'}
+                </Button>
+              </>
+            )}
+            {drawingStatus === 'after' && (
+              <Button className="w-full rounded-[3.125rem] border">
+                상점가기
+              </Button>
+            )}
+          </div>
         </div>
       </div>
     </>
