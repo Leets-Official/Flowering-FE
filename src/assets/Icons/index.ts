6090f1772e72800577b32d6dbd4335e5b7e24553
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-10 19:36:40 +0900

    feat: DrawIcon 추가

--- src/assets/Icons/index.ts
+++ src/assets/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeIcon } from './home.svg?react'
 export { default as CoinIcon } from './coin.svg?react'
 export { default as SideBarIcon } from './sidebar.svg?react'
+export { default as DrawIcon } from './draw.svg?react'
