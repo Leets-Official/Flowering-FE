be8628546323e1dcedc7942e0204b950560c7f19
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-12 22:32:15 +0900

    feat: 메인페이지에 헤더 추가

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,23 +1,25 @@
-import { Sidebar } from '@/components'
+import { Sidebar, Header } from '@/components'
 import { useState } from 'react'
 
 const MainPage = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
 
-  const handleSidebarToggle = () => {
-    setSidebarOpen(!sidebarOpen)
-  }
+  // const handleSidebarToggle = () => {
+  //   setSidebarOpen(!sidebarOpen)
+  // }
 
   const handleCloseSidebar = () => {
     setSidebarOpen(false)
   }
 
+  const icons = [{ name: 'DrawIcon' }, { name: 'SidebarIcon' }]
+
   return (
-    <section className="desktop:w-[375px]">
+    <section>
       <div
         className={`${sidebarOpen ? 'h-screen bg-gray-200 backdrop-blur' : ''}`}
       >
-        <div className={`flex ${sidebarOpen ? 'h-screen blur-sm' : ''}`}>
+        {/* <div className={`flex ${sidebarOpen ? 'h-screen blur-sm' : ''}`}>
           <h1>MainPage</h1>
           <button className="ml-auto" onClick={handleSidebarToggle}>
             사이드바
@@ -28,7 +30,14 @@ const MainPage = () => {
               onClick={handleCloseSidebar}
             />
           )}
-        </div>
+        </div> */}
+        {sidebarOpen && (
+          <div
+            className="fixed left-0 top-0 z-40 h-screen w-full blur"
+            onClick={handleCloseSidebar}
+          />
+        )}
+        <Header icons={icons} position="both" setSidebarOpen={setSidebarOpen} />
         <div className="relative">
           <Sidebar isOpen={sidebarOpen} />
         </div>
