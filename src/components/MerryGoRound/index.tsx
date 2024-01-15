349641f9377c0e3724caf2b351541abf4e29af8f
ggome1 <bookincar@naver.com>
2024-01-15 14:11:01 +0900

    refactor: nav 위치 수정

--- src/components/MerryGoRound/index.tsx
+++ src/components/MerryGoRound/index.tsx
@@ -62,7 +62,7 @@ const theme: Theme = {
         </button>
       ),
       navigation: ({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-3 left-2/4 z-50 flex -translate-x-2/4 gap-3">
+        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-3">
           {new Array(length).fill('').map((_, i) => (
             <span
               key={i}
