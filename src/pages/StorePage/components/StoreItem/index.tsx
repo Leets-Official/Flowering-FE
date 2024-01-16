6d878ca8582b4b66e17b6492df541e19eeaacd0a
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-16 14:48:23 +0900

    feat: 보유 코인에 따른 구매 슈량 제한 및 버튼 비활성화

--- src/pages/StorePage/components/StoreItem/index.tsx
+++ src/pages/StorePage/components/StoreItem/index.tsx
@@ -10,9 +10,19 @@ interface StoreItemProps {
 const StoreItem = ({ itemId, coin }: StoreItemProps) => {
   const navigate = useNavigate()
   const [itemNum, setItemNum] = useState<number>(0)
+  const maxAllowedQuantity = Math.round(3000 / parseInt(coin, 10))
 
   const itemNumHandler = (amount: number) => {
-    setItemNum((prevItemNum) => Math.max(0, prevItemNum + amount))
+    setItemNum((prevItemNum) => {
+      const newItemNum = Math.max(0, prevItemNum + amount)
+
+      // 보유 코인이 들어갈 예정
+      if (newItemNum <= maxAllowedQuantity) {
+        return newItemNum
+      } else {
+        return prevItemNum
+      }
+    })
   }
 
   return (
@@ -25,7 +35,7 @@ const StoreItem = ({ itemId, coin }: StoreItemProps) => {
           <p className="font-lg">포인세티아</p>
           <div className="font-xs flex items-center gap-1">
             <CoinIcon className="h-[14px] w-[14px]" />
-            <p>300 코인</p>
+            <p>{coin} 코인</p>
           </div>
           <FlowerIcon className="my-16 rotate-45" />
           <div className="flex gap-3">
@@ -37,16 +47,24 @@ const StoreItem = ({ itemId, coin }: StoreItemProps) => {
             </button>
             <p className=" font-xs text-[#000000]">{itemNum}개</p>
             <button
-              className="flex h-[16px] w-[16px] items-center justify-center rounded-full bg-[#959595] pt-0.5 font-normal text-white"
+              className={`flex h-[16px] w-[16px] items-center justify-center rounded-full ${
+                itemNum < maxAllowedQuantity ? 'bg-[#959595]' : 'bg-[#CCCCCC]'
+              } pt-0.5 font-normal text-white`}
               onClick={() => itemNumHandler(1)}
             >
               +
             </button>
           </div>
         </div>
-        <Button key="btn" onClick={() => navigate('/purchase')}>
-          구매하기
-        </Button>
+        {itemNum > 0 ? (
+          <Button key="btn" onClick={() => navigate('/purchase')}>
+            구매하기
+          </Button>
+        ) : (
+          <Button key="btn" disabled>
+            구매하기
+          </Button>
+        )}
       </Modal>
 
       <div className="mt-1 flex justify-center gap-1">
