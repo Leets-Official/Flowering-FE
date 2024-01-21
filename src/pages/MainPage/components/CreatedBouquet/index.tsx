226c02e84fde31cc8111bbfe3cc73cee32aec60a
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-22 01:42:28 +0900

    refactor: 꽃다발 빈 배열 조건 처리

--- src/pages/MainPage/components/CreatedBouquet/index.tsx
+++ src/pages/MainPage/components/CreatedBouquet/index.tsx
@@ -51,40 +51,43 @@ const CreatedBouquet = ({ bouquetInfo }: CreatedBouquetProps) => {
       </div>
       <div className="relative h-full w-full">
         <MarryGoRound setCurrentFlowerIndex={setCurrentFlowerIndex}>
-          {bouquetInfo.bouquets.map((bouquet) => (
-            <div
-              key={bouquet.bouquetId}
-              className="relative flex h-full w-full items-center justify-center"
-            >
-              <FlowerFrame flowers={bouquet.flowers} />
-              <div className="relative h-full w-[70%]">
-                <div className="absolute left-1/2 top-1/2 z-[15] w-full -translate-x-[60.5%] -translate-y-[32%]">
-                  <WrapperFrontLeftImage className="h-full w-full" />
+          {bouquetInfo.bouquets.map(
+            (bouquet) =>
+              bouquet.flowers.length > 0 && (
+                <div
+                  key={bouquet.bouquetId}
+                  className="relative flex h-full w-full items-center justify-center"
+                >
+                  <FlowerFrame flowers={bouquet.flowers} />
+                  <div className="relative h-full w-[70%]">
+                    <div className="absolute left-1/2 top-1/2 z-[15] w-full -translate-x-[60.5%] -translate-y-[32%]">
+                      <WrapperFrontLeftImage className="h-full w-full" />
+                    </div>
+                    <div className="absolute left-1/2 top-1/2 z-30 w-full -translate-x-[48.5%] -translate-y-[16%]">
+                      <WrapperFrontRightImage className="h-full w-full" />
+                    </div>
+                    <div className="absolute left-1/2 top-1/2 z-40 w-[45%] -translate-x-[20%] translate-y-[35%]">
+                      <p className="absolute left-1/3 top-1/3 flex  -translate-x-[75%] -translate-y-[15%] -rotate-[5deg] gap-px font-bodoni text-xs text-[#FFA6EE] sm:text-sm md:text-base desktop:text-sm">
+                        <span>{currentFlowerIndex + 1}</span>
+                        <span>
+                          {ordinalNum[currentFlowerIndex]
+                            ? ordinalNum[currentFlowerIndex]
+                            : 'th'}
+                        </span>
+                      </p>
+                      <img
+                        src={getImageUrl('flower_card')}
+                        alt="flower_card"
+                        className="w-full"
+                      />
+                    </div>
+                  </div>
+                  <div className="absolute left-1/2 top-1/2 z-0 w-[70%] -translate-x-[48%] -translate-y-[62%]">
+                    <WrapperBackImage className="h-full w-full" />
+                  </div>
                 </div>
-                <div className="absolute left-1/2 top-1/2 z-30 w-full -translate-x-[48.5%] -translate-y-[16%]">
-                  <WrapperFrontRightImage className="h-full w-full" />
-                </div>
-                <div className="absolute left-1/2 top-1/2 z-40 w-[45%] -translate-x-[20%] translate-y-[35%]">
-                  <p className="font-bodoni absolute left-1/3 top-1/3  flex -translate-x-[75%] -translate-y-[15%] -rotate-[5deg] gap-px text-xs text-[#FFA6EE] sm:text-sm md:text-base desktop:text-sm">
-                    <span>{currentFlowerIndex + 1}</span>
-                    <span>
-                      {ordinalNum[currentFlowerIndex]
-                        ? ordinalNum[currentFlowerIndex]
-                        : 'th'}
-                    </span>
-                  </p>
-                  <img
-                    src={getImageUrl('flower_card')}
-                    alt="flower_card"
-                    className="w-full"
-                  />
-                </div>
-              </div>
-              <div className="absolute left-1/2 top-1/2 z-0 w-[70%] -translate-x-[48%] -translate-y-[62%]">
-                <WrapperBackImage className="h-full w-full" />
-              </div>
-            </div>
-          ))}
+              ),
+          )}
         </MarryGoRound>
       </div>
       <footer className="absolute bottom-0 z-30 flex h-[5rem] w-full shrink-0 items-center justify-center px-6">
