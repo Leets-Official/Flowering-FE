8d7e79c5f6c5b8c3800050a8c17de69ac5d07304
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-23 01:02:45 +0900

    refactor: 받은편지 flower api 가져오기

--- src/pages/MyPage/components/ReceivedLetter/index.tsx
+++ src/pages/MyPage/components/ReceivedLetter/index.tsx
@@ -1,12 +1,16 @@
 import { Header } from '@/components'
 import { ICONS } from '@/constants'
+import { useGetFlower } from '@/apis/hooks'
 
 interface ReceivedLetterProps {
-  sender?: string
-  flowerId?: number
+  sender: string
+  flowerId: number
   onClose: () => void
 }
-const ReceivedLetter = ({ sender, onClose }: ReceivedLetterProps) => {
+const ReceivedLetter = ({ flowerId, sender, onClose }: ReceivedLetterProps) => {
+  const { data } = useGetFlower({ id: flowerId })
+  const letter = data?.data.letter
+
   return (
     <div className="absolute left-0 top-0 z-50 h-screen w-full transform overflow-hidden bg-white text-gray-300 transition-transform">
       <Header rightIcon={ICONS.CLOSE} onClose={onClose} />
@@ -15,7 +19,7 @@ const ReceivedLetter = ({ sender, onClose }: ReceivedLetterProps) => {
         <p className="text-gray-300">님이 보낸 편지에요</p>
       </div>
       <div className="font-ls mt-10 flex h-screen w-full flex-col rounded-t-[50px] bg-[#DADADA] px-7 pt-16">
-        <p className="mb-10">letter data</p>
+        <p className="mb-10">{letter}</p>
         <p className="text-right">2024년 01월 16일</p>
       </div>
     </div>
