274cb63657ed6eaf48535f3357082127abc861d6
ggome1 <bookincar@naver.com>
2024-01-23 02:49:45 +0900

    refactor: 회원가입 api 연동 및 바로 메인화면 이동

---
+++ src/pages/CreateBouquetPage/index.tsx
@@ -0,0 +1,82 @@
+import { useNavigate } from 'react-router'
+import { ICONS } from '@/constants'
+import { DDay, Name, Ribbon, Wrapper } from './components'
+import { useEffect, useState } from 'react'
+import { Button, Header } from '@/components'
+import { usePostBouquet } from '@/apis/hooks'
+
+const CreateBouquetPage = () => {
+  const navigate = useNavigate()
+  const [step, setStep] = useState(1)
+  const [wrapper, setWrapper] = useState('')
+  const [ribbon, setRibbon] = useState('')
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [activeButton, setActiveButton] = useState(false)
+  const { mutate: postBouquet } = usePostBouquet()
+
+  const handleClick = () => {
+    if (step === 1) setStep(2)
+    else if (step === 2) setStep(3)
+    else if (step === 3) setStep(4)
+    else if (step === 4) {
+      const props = { bouquetName: name, wrapper, ribbon, dday: date }
+      postBouquet(props, {
+        onSuccess: (data) => {
+          console.log(data)
+        },
+      })
+      navigate('/create-bouquet')
+    }
+    setActiveButton(false)
+  }
+
+  useEffect(() => {
+    if (
+      (step === 1 && wrapper) ||
+      (step === 2 && ribbon) ||
+      (step === 3 && name && name.length < 10) ||
+      (step === 4 && date)
+    ) {
+      setActiveButton(true)
+    } else {
+      setActiveButton(false)
+    }
+  }, [wrapper, ribbon, name, date, setActiveButton, step])
+
+  return (
+    <div className="flex h-full flex-col overflow-hidden">
+      <Header
+        leftIcon={ICONS.GOBACK}
+        onGoBack={() => {
+          if (step === 1) {
+            navigate(-1)
+          } else {
+            setStep(step - 1)
+          }
+        }}
+      />
+      <div className="h-full">
+        {step === 1 && <Wrapper wrapper={wrapper} setWrapper={setWrapper} />}
+        {step === 2 && (
+          <Ribbon wrapper={wrapper} ribbon={ribbon} setRibbon={setRibbon} />
+        )}
+        {step === 3 && <Name name={name} setName={setName} />}
+        {step === 4 && <DDay setDDay={setDate} />}
+      </div>
+      <div className="font-md px-6 py-5">
+        <Button
+          onClick={handleClick}
+          className={`${
+            activeButton ? 'bg-black text-white' : 'bg-[#DDDDDD] text-[#282828]'
+          }`}
+          disabled={!activeButton}
+        >
+          {step < 4 ? '다음' : '완료'}
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default CreateBouquetPage
