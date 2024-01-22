52adaaed50639baad1191763f3e7738dece83da9
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-23 06:10:01 +0900

    feat: 메인페이지 쿼리스트링 기능 구현

--- src/apis/hooks/useGetBouquet.ts
+++ src/apis/hooks/useGetBouquet.ts
@@ -2,11 +2,15 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 import apiClient from '../apiClient'
 import type { BouquetInfo } from '@/types'
 
+interface Response {
+  data: BouquetInfo
+}
+
 const useGetBouquet = ({ id }: { id: string }) => {
   const getBouquet = async () => {
-    const response = await apiClient.get<BouquetInfo>(`/bouquet/${id}`)
+    const response = await apiClient.get<Response>(`/bouquet/${id}`)
 
-    return response.data
+    return response.data.data
   }
 
   return useSuspenseQuery({
