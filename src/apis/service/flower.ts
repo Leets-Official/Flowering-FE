5e953e832ab1e98feba8d358cb9eb8581593e1e7
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-23 12:04:28 +0900

    refactor: 불필요한 코드 삭제

--- src/apis/service/flower.ts
+++ src/apis/service/flower.ts
@@ -2,7 +2,7 @@ import apiClient from '@/apis/apiClient.ts'
 
 const flower = async ({ id }: { id: number }) => {
   try {
-    const response = await apiClient.get('/flower', { params: { id: id } })
+    const response = await apiClient.get('/flower', { params: { id } })
 
     return response.data
   } catch (error) {
