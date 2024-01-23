f52cc77e5d3cdac28560a06a4f552bd8e5d6e3bd
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-23 12:41:41 +0900

    refactor: git conflict

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -5,7 +5,7 @@ interface ItemProps {
 const Item = ({ id }: ItemProps) => {
   return (
     <div className={`h-[72px] w-[72px]`}>
-      <img src={`/src/assets/images/${id}.png`} />
+      <img src={`/src/assets/images/items/${id}.png`} />
     </div>
   )
 }
