85dc7f5e42c75678b4678c4635a667d460436c7e
hayamaster <dnfxmfkdbzos@naver.com>
2024-01-28 16:00:57 +0900

    refactor: svg파일 수정

--- src/assets/images/flowerFrame/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as 청량한_한여름밤의_꿈 } from './청량한_한여름밤의_꿈.svg?react'
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,2 @@
 export * from './wrapping'
 export { default as NoSelectItem } from './no_select_item.svg?react'
-export * from './flowerFrame'
