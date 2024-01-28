429cbe2549d2cb16fc614cb304878b566b3777ec
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-28 12:56:32 +0900

    feat: 로그인여부에 따른 사이드바 수정

--- src/components/Sidebar/Box/index.tsx
+++ src/components/Sidebar/Box/index.tsx
@@ -1,15 +1,25 @@
 import Content from '@/components/Sidebar/Content'
-import { Link } from 'react-router-dom'
+import useGetUserInfo from '@/apis/hooks/useGetUser.ts'
+import { useNavigate } from 'react-router-dom'
 
 const Box = () => {
+  const navigate = useNavigate()
+  const { data } = useGetUserInfo()
+  if (!data) {
+    return <p>error</p>
+  }
+  const { nickname } = data
+
   return (
     <>
       <div className="flex h-screen w-[315px] flex-col gap-14 rounded-l-[50px] bg-white pl-10 pt-12">
-        <Link to="/login">
-          <p className="font-lg text-[22px] text-gray-300">
-            로그인 후 사용하세요!
-          </p>
-        </Link>
+        <p
+          onClick={() => nickname && navigate('/login')}
+          className="font-lg text-[22px] text-gray-300"
+        >
+          {nickname ? `${nickname}님` : '로그인 후 사용하세요'}
+        </p>
+
         <div className="flex flex-col gap-8 text-gray-300">
           <Content content="MY" route="/mypage" />
           <Content content="SHOP" route="/store" />
