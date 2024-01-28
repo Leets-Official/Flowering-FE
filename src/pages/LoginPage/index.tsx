4adbdc65a8c707442ac7071ce95b911d33cf2e6f
ggome1 <bookincar@naver.com>
2024-01-28 13:06:30 +0900

    test

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -3,12 +3,12 @@ import logo from '@/assets/images/fling.png'
 import MerryGoRound from '@/components/MerryGoRound'
 
 const LoginPage = () => {
-  // const LoginAssign = () => {
-  //   const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/oauth`
-  //   const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID
-  //   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
-  //   window.location.href = KAKAO_AUTH_URL
-  // }
+  const LoginAssign = () => {
+    const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/oauth`
+    const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
+    window.location.href = KAKAO_AUTH_URL
+  }
 
   return (
     <div className="flex h-full flex-col items-center">
@@ -21,7 +21,7 @@ const LoginPage = () => {
           SNS로 간편하게 가입하기
         </div>
         <button
-          // onClick={LoginAssign}
+          onClick={LoginAssign}
           className="font-base flex h-[45px] items-center justify-center gap-2 rounded-[3rem] bg-[#FEE500]"
         >
           <div className="mt-1 flex">
