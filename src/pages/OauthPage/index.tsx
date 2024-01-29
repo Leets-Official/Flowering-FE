36c2e6949dff0a17a64c82eaa7652b76f25b5043
ggome1 <bookincar@naver.com>
2024-01-29 17:57:13 +0900

    refactor: 로그인페이지 이동 시 토큰 삭제

--- src/pages/OauthPage/index.tsx
+++ src/pages/OauthPage/index.tsx
@@ -22,6 +22,7 @@ const OauthPage = () => {
         }
         postLogin(props, {
           onSuccess: (data) => {
+            localStorage.removeItem('kakaoItem')
             localStorage.setItem(
               'accessToken',
               data.data.data.token.accessToken,
