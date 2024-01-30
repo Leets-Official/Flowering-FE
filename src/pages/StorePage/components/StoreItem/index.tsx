b7db2689afa71795730366e87f546a6cc87f91ae
은지 <81403265+eunji0714@users.noreply.github.com>
2024-01-30 17:58:36 +0900

    refactor: 구매 확정 시 모달 닫힘

--- src/pages/StorePage/components/StoreItem/index.tsx
+++ src/pages/StorePage/components/StoreItem/index.tsx
@@ -27,6 +27,7 @@ const StoreItem = ({
 }: StoreItemProps) => {
   const [itemNum, setItemNum] = useState<number>(0)
   const [confirmPurchase, setConfirmPurchase] = useState<boolean>(false)
+  const [modalVisible, setModalVisible] = useState<boolean>(true)
   const maxAllowedQuantity =
     coin && type !== 'deco' ? Math.round(coin / parseInt(price, 10)) : 1
   const { storeFlowerMutation } = usePostStoreFlower(setConfirmPurchase)
@@ -42,6 +43,7 @@ const StoreItem = ({
     if (type === 'deco' && itemNum > 0) {
       storeDecoMutation.mutate({ decoId: itemId })
     }
+    setModalVisible(false)
   }
   const itemNumHandler = (amount: number) => {
     setItemNum((prevItemNum) => {
@@ -57,53 +59,61 @@ const StoreItem = ({
 
   return (
     <div tabIndex={-1}>
-      <Modal>
-        <div className="flex cursor-pointer justify-center" key="toggle">
-          {<Item className={`${owned ? 'grayscale' : ''}`} name={itemName} />}
-        </div>
-        <div className="flex flex-col items-center" key="content">
-          <p className="font-lg text-gray-300">{itemName.replace(/-/g, ' ')}</p>
-          <div className="font-xs flex items-center gap-1">
-            <CoinIcon className="h-[14px] w-[14px]" />
-            <p>{price} 코인</p>
+      {modalVisible && (
+        <Modal>
+          <div className="flex cursor-pointer justify-center" key="toggle">
+            {<Item className={`${owned ? 'grayscale' : ''}`} name={itemName} />}
           </div>
-          <BigItem className="h-[190px] w-[133px]" name={itemName} />
-          {owned ? (
-            <p className="font-lg text-gray-300">이미 보유하신 아이템입니다.</p>
-          ) : (
-            <>
-              <div className="mb-5 flex gap-3">
-                <button
-                  className={`flex h-[16px] w-[16px] items-center justify-center rounded-full ${
-                    itemNum >= 1 ? 'bg-gray-200' : 'bg-gray-100'
-                  } font-normal text-white`}
-                  onClick={() => itemNumHandler(-1)}
-                >
-                  -
-                </button>
-                <p className=" font-xs text-[#000000]">{itemNum}개</p>
-                <button
-                  className={`flex h-[16px] w-[16px] items-center justify-center rounded-full ${
-                    itemNum < maxAllowedQuantity ? 'bg-gray-200' : 'bg-gray-100'
-                  } font-normal text-white`}
-                  onClick={() => itemNumHandler(1)}
-                >
-                  +
-                </button>
+          <div className="flex flex-col items-center" key="content">
+            <p className="font-lg text-gray-300">
+              {itemName.replace(/-/g, ' ')}
+            </p>
+            <div className="font-xs flex items-center gap-1">
+              <CoinIcon className="h-[14px] w-[14px]" />
+              <p>{price} 코인</p>
+            </div>
+            <BigItem className="h-[190px] w-[133px]" name={itemName} />
+            {owned ? (
+              <p className="font-lg text-gray-300">
+                이미 보유하신 아이템입니다.
+              </p>
+            ) : (
+              <div>
+                <div className="mb-5 flex gap-3">
+                  <button
+                    className={`flex h-[16px] w-[16px] items-center justify-center rounded-full ${
+                      itemNum >= 1 ? 'bg-gray-200' : 'bg-gray-100'
+                    } font-normal text-white`}
+                    onClick={() => itemNumHandler(-1)}
+                  >
+                    -
+                  </button>
+                  <p className=" font-xs text-[#000000]">{itemNum}개</p>
+                  <button
+                    className={`flex h-[16px] w-[16px] items-center justify-center rounded-full ${
+                      itemNum < maxAllowedQuantity
+                        ? 'bg-gray-200'
+                        : 'bg-gray-100'
+                    } font-normal text-white`}
+                    onClick={() => itemNumHandler(1)}
+                  >
+                    +
+                  </button>
+                </div>
+                {itemNum > 0 ? (
+                  <Button key="btn" onClick={onPurchaseButton}>
+                    구매하기
+                  </Button>
+                ) : (
+                  <Button key="btn" disabled>
+                    구매하기
+                  </Button>
+                )}
               </div>
-              {itemNum > 0 ? (
-                <Button key="btn" onClick={onPurchaseButton}>
-                  구매하기
-                </Button>
-              ) : (
-                <Button key="btn" disabled>
-                  구매하기
-                </Button>
-              )}
-            </>
-          )}
-        </div>
-      </Modal>
+            )}
+          </div>
+        </Modal>
+      )}
       <div className="mt-1 flex justify-center gap-1">
         <CoinIcon className="h-[13px] w-[13px]" />
         <p className="font-xs text-gray-200">{price}</p>
