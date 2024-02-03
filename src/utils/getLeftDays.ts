64b52eed8e6cc09cf5afd7f75733485276a0a9f1
hayamaster <dnfxmfkdbzos@naver.com>
2024-02-03 09:01:17 +0900

    fix: getLeftDays 함수 수정

--- src/utils/getLeftDays.ts
+++ src/utils/getLeftDays.ts
@@ -2,7 +2,7 @@ const getLeftDays = (graduationDate: string) => {
   if (graduationDate === '') return -1
 
   const today = new Date()
-  const graduation = new Date(graduationDate)
+  const graduation = new Date(graduationDate.replace(/-/g, '/'))
   const diff = graduation.getTime() - today.getTime()
   const leftDays = Math.floor(diff / (1000 * 60 * 60 * 24))
 
